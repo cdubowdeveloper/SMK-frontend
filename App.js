12cb80676c07cdadbd072db1f115b24b2979d581
caleb dubow <cdubowdeveloper@gmail.com>
2021-11-15 17:30:39 -0600

    login page

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Login</Text>
+      <TextInput 
+        placeholder="Username"
+        style={styles.username}
+      />
+      <TextInput  
+        placeholder="Password"
+        style={styles.password}
+        secureTextEntry={true}
+      />
+
+      <Button 
+        style={styles.loginButton}
+        title="Login"
+      />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +33,29 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  username: {
+      height: 40,
+      margin: 12,
+      borderWidth: 1,
+      padding: 10,
+      textAlign: 'center'
+  },
+
+  password: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    textAlign: 'center'
+  },
+
+  loginButton: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    textAlign: 'center'
+  }
 });
+
